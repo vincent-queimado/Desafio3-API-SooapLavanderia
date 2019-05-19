30508d325bb90a841eac30f60cc455850663e1ae
Everguard Dev <48055990+vincent-queimado@users.noreply.github.com>
2019-05-19 19:13:45 -0300

    Add files via upload

--- orders/order.model.js
+++ orders/order.model.js
@@ -1,13 +1,15 @@
 //Importação de conector e schema de banco de dados via Mangoose 
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
+
 
 //Definição do documento do pedido e campos obrigatorios
 const schema = new Schema({
     userId: { type: String, required: true },
     clothes: { type: String, required: true },
     schedule: { type: Date, required: true },
-    createdDate: { type: Date, default: Date.now }
+    createdDate: { type: Date, default: Date.now() }
 });
 
 schema.set('toJSON', { virtuals: true });
--- orders/order.service.js
+++ orders/order.service.js
@@ -2,8 +2,15 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const db = require('_helpers/db');
+const moment = require('moment');
+const momenttz = require('moment-timezone');
+
 const Order = db.Order;
 const User = db.User;
+
+const lapsTime = 30;
+const cars = 2;
+const daysCars = { 1: 1, 2: 2, 3: 2, 4: 2, 5: 2, 6: 1, 7: 1 }; //Seg. a Sex. vs Qtde de Veiculos
 
 module.exports = {
     getAll,
@@ -22,25 +29,61 @@ async function getById(id) {
 }
 
 async function create(orderParam) {
+    var count = null;
+
     console.log(orderParam);
 
-    if (orderParam.userId.match(/^[0-9a-fA-F]{24}$/)) {
-
-        if (await !User.findById(orderParam.userId)) {
-            throw 'Usuário não existe. O pedido não pode ser cadastrado!';
-        } else {
-            // if (await Order.findOne({ schedule: orderParam.schedule })) {
-            //     throw 'Sem disponibilidade de veículo para coleta neste horário. Escolha outro horário! ';
-            // }
-
-            const order = new Order(orderParam);
-
-            await order.save();
-
-        }
-    } else {
-        throw 'Usuário invalido. O pedido não pode ser cadastrado!';
+    // Verifica se o ID do usuario é valido de tipo objeto
+    if (await !orderParam.userId.match(/^[0-9a-fA-F]{24}$/)) {
+        throw 'Usuário inválido. O pedido não pode ser cadastrado!';
     }
+
+    // Verifica se o ID do usuario exsite no banco de dados
+    if (await !User.findById(orderParam.userId)) {
+        throw 'Usuário não existe. O pedido não pode ser cadastrado!';
+    }
+
+    // Recupera a data de solicitação de coleta, adiciona e substrai 30 minutes
+    var dateRecife = moment(orderParam.schedule).utcOffset('-0300').format("YYYY-MM-DDTHH:mm:ss.SSSZ")
+    var dateStart = moment(dateRecife).subtract(lapsTime, 'minutes').format('YYYY-MM-DDTHH:mm:ss.SSSZ'); // 30 minutes antes
+    var dateStop = moment(dateRecife).add(lapsTime, 'minutes').format('YYYY-MM-DDTHH:mm:ss.SSSZ'); // 30 minutes depois
+    var dow = moment(orderParam.schedule).isoWeekday();
+
+    console.log("Range Inicio: " + dateStart);
+    console.log("Range Fim: " + dateStop);
+    console.log("Dia da semana: " + dow);
+    console.log("Veiculos disponiveis:" + daysCars[dow]);
+
+
+    // Verifica no banco de dados quantos registros de solicitação existem nesse range de tempo
+    if (await Order.find({ schedule: { $gte: dateStart, $lte: dateStop } }).exec(function(err, results) { //Busca quantos 
+            console.log(results.length);
+        }));
+
+
+
+    if (await count == 0) {
+        console.log("Nenhum pedido encontrado entre " + moment(dateStart).format('YYYY-MM-DD HH:mm') + " a " + moment(dateStop).format('YYYY-MM-DD HH:mm') + ".");
+    }
+
+    if (await count >= cars) {
+        console.log("Veiculos não disponiveis para coleta neste horario.");
+        throw 'Sem disponibilidade de veículo para coleta neste horário. Escolha outro horário! ';
+    }
+
+    if (await count > 0 && count < cars) {
+        var tmp_cars = daysCars[dow];
+        if (count >= tmp_cars) {
+            console.log("Veiculos não disponiveis para coleta neste horario.");
+            throw 'Sem disponibilidade de veículo para coleta neste horário. Escolha outro horário! ';
+        }
+    }
+
+
+    console.log("Pedido cadastrado com sucesso!");
+
+    const order = new Order(orderParam);
+    await order.save();
 
 }
 
